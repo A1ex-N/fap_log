d53d4c7b4e825e61d513c0bd512f4c07c777418b
A1ex-N <azander724@gmail.com>
2022-05-30 19:30:41 +1000

    Removed unnecessary comments

--- duration.py
+++ duration.py
@@ -45,21 +45,6 @@ for time in times:
     total_time += float(time)
 
 
-'''
-    This is where it becomes spaghetti code. Before adding
-    this part, the number after the decimal point could
-    reach over 60, but there's only 60 minutes in an hour.
-    with this code we use modulus to check if the number is greater
-    than 60. So for example if the number is 82
-    82 % 60 = 12. We just add one to the first number and set the
-    second number to the remainder. I *think* this gives us an
-    accurate time???????
-
-    EXAMPLE:
-        before: 214.87
-        after: 215.27
-'''
-
 total_time = round(total_time, 2)
 first_place = int(str(total_time).split('.')[0])
 second_place = int(str(total_time).split('.')[1])
@@ -73,14 +58,6 @@ if int(second_place) > 60:
     total_time = total_time.replace(' ', '')
     total_time = total_time.replace('[', '')
     total_time = total_time.replace(']', '')
-
-'''
-    The reason is says "give or take" is because there's a few entries in my fap.log that
-    weren't added automatically but i did add a time to. For example one line says i jerked
-    for about 4 hours, don't know how accurate that is. Also another one says about half
-    an hour. Also all the times i've jerked off without using the fap_logger script prob
-    adds up to at least a couple of hours. So this total time is not 100% accurate.
-'''
 
 lines_to_search = lines_to_search.split('\n')
 if year == None:
